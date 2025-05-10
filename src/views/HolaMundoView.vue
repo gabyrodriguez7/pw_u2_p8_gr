2b9e0f06b86d5a5d85fea4bc6849cba3c87d8539
gabyrodriguez7 <glrodriguezc@uce.du.ec>
2025-05-09 21:50:24 -0500

    Mi primer proyecto Vue.js

---
+++ src/views/HolaMundoView.vue
@@ -0,0 +1,17 @@
+<template>
+    <HolaMundo></HolaMundo>
+</template>
+
+<script>
+import HolaMundo from '../components/HolaMundo.vue';
+
+export default {
+    components: {
+        HolaMundo,
+    },
+
+}
+</script>
+
+<style>
+</style>
